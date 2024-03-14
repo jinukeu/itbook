0fec8e7caf4dea4a1f5cff1c89e1d5e8a4a9ee3a
jinukeu <koownij@kakao.com>
2024-03-14 20:14:08 +0900

    feat/#7: LazyGridState.kt 추가

--- core/ui/src/main/java/com/jinukeu/core/ui/extension/LazyGridState.kt
+++ core/ui/src/main/java/com/jinukeu/core/ui/extension/LazyGridState.kt
@@ -1,15 +1,16 @@
 package com.jinukeu.core.ui.extension
 
-import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
+import kotlinx.coroutines.flow.collectLatest
 
 // https://manavtamboli.medium.com/infinite-list-paged-list-in-jetpack-compose-b10fc7e74768
 @Composable
-fun LazyListState.OnBottomReached(
+fun LazyGridState.OnBottomReached(
   // tells how many items before we reach the bottom of the list
   // to call onLoadMore function
   buffer: Int = 0,
@@ -26,15 +27,12 @@ fun LazyListState.OnBottomReached(
       lastVisibleItem.index >= layoutInfo.totalItemsCount - 1 - buffer
     }
   }
-
   LaunchedEffect(shouldLoadMore) {
     snapshotFlow { shouldLoadMore.value }
-      .collect {
+      .collectLatest {
         if (it) {
           onLoadMore()
         }
       }
   }
 }
-
-fun LazyListState.isScrolledToEnd() = layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
